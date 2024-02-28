1663f9edcec497e13bafcac7d4ff99b95f0e4af7
DylanMlilo <121649259+dylanmlilo@users.noreply.github.com>
2024-02-28 10:39:10 +0000

    Rendering dynamic content

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('home.html')
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bulawayo, Zimbabwe',
+        'salary': '$1500',
+    },
+    {
+        'id': 2,
+        'title': 'Front End Developer',
+        'location': 'Harare, Zimbabwe',
+        'salary': '$1000',
+    },
+    {
+        'id': 3,
+        'title': 'Back End Developer',
+        'location': 'Bulawayo, Zimbabwe',
+        'salary': '$1300',
+    },
+    {
+        'id': 4,
+        'title': 'Data Scientist',
+        'location': 'Mutare, Zimbabwe',
+        'salary': '$2000',
+    }
+]
+
+@app.route('/')
+def index():
+    return render_template('home.html', jobs=JOBS, company_name='Nando')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
